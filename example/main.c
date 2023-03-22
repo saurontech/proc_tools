00d703092678a5528932640ab61cdaced05a6b87
samuel cheng <samuel.cheng@advantech.com.tw>
2023-03-22 10:42:33 +0800

    add loop

--- example/main.c
+++ example/main.c
@@ -8,6 +8,13 @@
 #include <unistd.h>
 #include "proc_tools.h"
 
+#ifdef LOOP
+struct _cmd_info{
+	int active;
+	char cmd[1024];
+};
+#endif
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -17,36 +24,51 @@ int main(int argc, char *argv[])
 	char cmd[1024];
 	int cmdlen;
 	struct stat sb;	
-		
+#ifdef LOOP
+	struct _cmd_info * cmds;
+
+	cmds = malloc(sizeof(struct _cmd_info)*(argc - 1));
+	memset(cmds, 0, sizeof(struct _cmd_info)*(argc - 1));
+
+	do{
+#endif
 	for(i = 1; i < argc; i++){
 		if(stat(argv[i], &sb) == 0){
 		}else if(__search_port_inode(atoi(argv[i]), 10, &sb.st_ino) == 0){
 		}else{
-			printf("no inode %s", argv[i]);
+			printf("no inode %s\n", argv[i]);
+#ifdef LOOP
+			if(cmds[i].active){
+				printf("reactivate %s\n", cmds[i].cmd);
+			}
+#endif
+			continue;
 		}
 
 		if(__cmd_inode_search_pid(0, sb.st_ino, 
 					cmd, sizeof(cmd), &cmdlen, 
 					&pid)){
 			printf("no ps operating %s\n", argv[i]);
+#ifdef LOOP
+			if(cmds[i].active){
+				printf("reactivate %s\n", cmds[i].cmd);
+			}
+#endif
 			continue;
 		}
-		
+#ifdef LOOP
+		cmds[i].active = 1;
+		memcpy(cmds[i].cmd, cmd, cmdlen);
+		cmds[i].cmd[cmdlen] = '\0';
+#endif	
 		printf("%s operating %s\n", cmd, argv[i]);
 	}
 
-/*	if(__search_port_inode(atoi(argv[1]), 10, &sb.st_ino) == 0 &&
-				__cmd_inode_search_pid(0, sb.st_ino, 
-					cmd, sizeof(cmd), &cmdlen, 
-					&pid) == 0){
-			printf("ret 2 = %d cmd %s\n", ret, cmd);
-	}else{
-		printf("no ps operating %s\n", argv[1]);
-	}
-
-*/
+#ifdef LOOP
+	sleep(10);
+	}while(1);
+#endif
 
 
 	return 0;
-
 }
